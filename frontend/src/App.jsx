689d0a4e8b05e993ccb25b9568c82ceb5a5a239b
Darwin Zamarca G <dzamarca@subsole.com>
2024-12-15 01:43:04 -0300

    change - implementa ruta protegida

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,29 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Home, LoginPage, RegistrerPages, Cart, Pizza } from './pages/index'
 import { Footer, Navbar, NotFound, Profile } from './views/index';
-import CardProvider from "./context/CartContext";
-import PizzasProvider from "./context/PizzasContext";
-import UserProvider from "./context/UserContext"
+import { UserContext } from "./context/UserContext";
+import { useContext } from "react";
+
 
 function App() {
+  const {token} = useContext(UserContext)
   return (
-    <BrowserRouter>
-      <UserProvider>
-        <PizzasProvider>
-          <CardProvider>
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/register" element={<RegistrerPages />} />
-              <Route path="login" element={<LoginPage />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="/pizza/:id" element={<Pizza />} />
-              <Route path="/404" element={<NotFound />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-            <Footer />
-          </CardProvider>
-        </PizzasProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={!token?<RegistrerPages />:<Navigate to='/'/>}/>
+        <Route path="login" element={!token?<LoginPage />:<Navigate to='/'/>}/>
+        <Route path="/profile" element={token?<Profile />:<Navigate to='/login'/>} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/pizza/:id" element={<Pizza />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </>
   )
 }
 
